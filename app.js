5aa15f80a6c2bf2389ca72a7193c798e2e04abdb
Glorious-TaehanL <rendezvous.ted@gmail.com>
2023-03-21 20:35:39 +0900

    update error handler and jwt

--- app.js
+++ app.js
@@ -3,20 +3,26 @@ const app = express();
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const connectDB = require('./db/connect');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
-
+app.use(express.static('./public'));
 app.use(express.json());
-app.use('/api/v1/tasks',require('./routes/task'));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use('/api/v1/tasks', require('./routes/task'));
+app.use(errorHandlerMiddleware);
 
-const port = 8080;
-const start = async() => {
+const port = process.env.PORT;
+const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port,console.log("Sever Listening ..."));
+    app.listen(port, console.log(`Sever Listening port number is : ${port}`));
   } catch (error) {
     console.log(error);
   }
 };
 
 start();
-
